2327851430ff0010af2caf80c5ac89c9314c4973
hien-pham-d <dh.pham.dev@gmail.com>
2025-04-30 00:56:18 +0900

    chore: add a keymap to eval expression elisp

--- home-manager/dotfiles/emacs/lisp/init-keymaps.el
+++ home-manager/dotfiles/emacs/lisp/init-keymaps.el
@@ -163,6 +163,7 @@ of the form (KEY COMMAND DESC)."
                            ("r" persp-state-load "session-restore")
                            ("x" eval-region "eval-selected")
                            ("X" eval-buffer "eval-file")
+                           (":" eval-expression "eval-expression")
                            ("q" save-buffers-kill-emacs "quit")
                            ("c" tab-bar-close-tab "tab-close")
                            )))
